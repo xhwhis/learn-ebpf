10e245c82edd2806f6dd3411e405575659fe238d
xhwhis <hi@whis.me>
2022-12-30 15:10:18 +0800

    feat: try to log payload

--- classifier/classifier-ebpf/src/main.rs
+++ classifier/classifier-ebpf/src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![allow(nonstandard_style, dead_code)]
 
 use core::mem;
 
@@ -11,13 +12,16 @@ use aya_bpf::{
 };
 use aya_log_ebpf::info;
 
-mod bindings;
-use bindings::{ethhdr, iphdr, tcphdr, udphdr};
+use memoffset::offset_of;
 
+mod bindings;
+use bindings::{ethhdr, iphdr, tcphdr};
+
+const ETH_P_IP: u16 = 0x0800;
+const IPPROTO_TCP: u8 = 6;
 const ETH_HDR_LEN: usize = mem::size_of::<ethhdr>();
 const IP_HDR_LEN: usize = mem::size_of::<iphdr>();
 const TCP_HDR_LEN: usize = mem::size_of::<tcphdr>();
-const UDP_HDR_LEN: usize = mem::size_of::<udphdr>();
 
 #[repr(C)]
 pub struct Buf {
@@ -36,6 +40,22 @@ pub fn classifier(ctx: TcContext) -> i32 {
 }
 
 fn try_classifier(ctx: TcContext) -> Result<i32, i32> {
+    let eth_type = ctx
+        .load::<u16>(offset_of!(ethhdr, h_proto))
+        .map_err(|_| TC_ACT_PIPE)?;
+    info!(&ctx, "eth_type: {}", eth_type);
+    if eth_type != ETH_P_IP {
+        return Ok(TC_ACT_PIPE);
+    }
+
+    let protocol = ctx
+        .load::<u8>(ETH_HDR_LEN + offset_of!(iphdr, protocol))
+        .map_err(|_| TC_ACT_PIPE)?;
+    info!(&ctx, "protocol: {}", protocol);
+    if protocol != IPPROTO_TCP {
+        return Ok(TC_ACT_PIPE);
+    }
+
     let buf = unsafe {
         let ptr = BUF.get_ptr_mut(0).ok_or(TC_ACT_PIPE)?;
         &mut *ptr
@@ -48,7 +68,7 @@ fn try_classifier(ctx: TcContext) -> Result<i32, i32> {
     // do something with `buf`
     unsafe {
         let a = &buf.buf[..len];
-        info!(&ctx, "{}", core::str::from_utf8_unchecked(a));
+        info!(&ctx, "payload: {}", core::str::from_utf8_unchecked(a));
     }
 
     Ok(TC_ACT_PIPE)
